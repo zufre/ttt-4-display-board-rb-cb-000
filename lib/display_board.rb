2fb534e3d2549843d4526814c725e98e126adb84
Cedric David <cedric.david@btopenworld.com>
2018-08-28 04:41:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,12 @@
 # out the current state.
 def display_board
 
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{{board[8]} "
 
 end
+
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
